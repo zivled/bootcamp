a21e5657bf171789f8666ea86403ed762b33467a
Ziv Lederer <zivled@rafael.co.il>
2021-10-21 11:29:39 +0300

    adding cleanup.sh

---
+++ cleanup.sh
@@ -0,0 +1,5 @@
+# post 
+vagrant destroy -f
+ssh-keygen -f "/home/zivled/.ssh/known_hosts" -R "172.28.128.203"
+ssh-keygen -f "/home/zivled/.ssh/known_hosts" -R "172.28.128.202"
+ssh-keygen -f "/home/zivled/.ssh/known_hosts" -R "172.28.128.201"
--- lab.sh
+++ lab.sh
@@ -6,4 +6,3 @@ sshpass -f password.txt ssh-copy-id vagrant@172.28.128.202
 sshpass -f password.txt ssh-copy-id vagrant@172.28.128.203
 ansible-playbook k8s_pre.yml -i inventory.yml -b
 
-# post 
